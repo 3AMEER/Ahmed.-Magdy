61f145af717ffa0402b5a3185e1853186908c571
Ahmed Magdy <83177544+3AMEER@users.noreply.github.com>
2022-10-17 23:21:39 +0200

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,42 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+    breakpoints: {
+    640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+    },
+    768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+    },
+    1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+    },
+    },
+});
+
+
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+    const triggerBottom = (window.innerHeight / 4) * 4;
+    boxes.forEach((box) =>{
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) box.classList.add("show");
+        else box.classList.remove("show")
+    });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes()
---
+++ page-projects/ui Design/style.js
@@ -0,0 +1,25 @@
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+    const triggerBottom = (window.innerHeight / 4) * 4;
+    boxes.forEach((box) =>{
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) box.classList.add("show");
+        else box.classList.remove("show")
+    });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes()
+
+
+// Start Action For Fav
+let icons = document.querySelectorAll(".love");
+
+for(let i = 0; i < icons.length; i++){
+    let love = icons[i];
+
+    love.onclick = function(){
+        love.style.cssText = "color: red;";
+    }
+}
